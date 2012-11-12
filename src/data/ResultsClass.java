7a41b1ece55025491255e940aab727bdc1805667
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2012-11-12 14:24:40 +0200

    Added GCP method

--- src/data/ResultsClass.java
+++ src/data/ResultsClass.java
@@ -9,6 +9,14 @@ public class ResultsClass extends ArrayList<EquivalenceClass> {
 	 */
 	private static final long serialVersionUID = 1L;
 	
+	public double getGCP(int qid[], int ranges[], int numberOfTuples){
+		double gcp=0;
+		for(EquivalenceClass eq:this)
+			gcp+=eq.getNCP(qid, ranges)*eq.getNumberOfTuples();
+		gcp=gcp/(qid.length*numberOfTuples*1.0);
+		return gcp;
+	}
+	
 	public String toString(){
 		String buffer="";
 		for(int i=0;i<this.size();i++)
