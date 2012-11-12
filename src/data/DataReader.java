d3e30246ef128fcae2b2e70fac1f3d10bbc074b1
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2012-11-12 12:18:07 +0200

    Added class for reading data from file

---
+++ src/data/DataReader.java
@@ -0,0 +1,37 @@
+package data;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class DataReader {
+
+	private String filename;
+	private BufferedReader file;
+	private EquivalenceClass set;
+
+	public DataReader(String filename) throws FileNotFoundException{
+		this.filename=filename;
+		file = new BufferedReader(new FileReader(this.filename));
+		set = new EquivalenceClass();
+		
+	}
+	
+	public EquivalenceClass getTuples() throws IOException{
+		while(this.file.ready())
+			this.set.add(new Tuple(this.file.readLine().split(",")));
+		this.file.close();
+		return this.set;
+	}
+	
+	public Tuple getNextTuple() throws IOException{
+		if(this.file.ready())
+			return new Tuple(this.file.readLine().split(","));
+		else{
+			this.file.close();
+			return null;
+		}
+			
+	}
+}
