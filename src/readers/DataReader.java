ba8972b4d7e7ded2bc426843848b0393add428e8
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2012-11-13 11:07:08 +0200

    changed package

---
+++ src/readers/DataReader.java
@@ -0,0 +1,40 @@
+package readers;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import data.EquivalenceClass;
+import data.Tuple;
+
+public class DataReader {
+
+	private String filename;
+	private BufferedReader file;
+	private EquivalenceClass set;
+
+	public DataReader(String filename) throws FileNotFoundException{
+		this.filename=filename;
+		this.file = new BufferedReader(new FileReader(this.filename));
+		this.set = new EquivalenceClass();
+		
+	}
+	
+	public EquivalenceClass getTuples() throws IOException{
+		while(this.file.ready())
+			this.set.add(new Tuple(this.file.readLine().split(",")));
+		this.file.close();
+		return this.set;
+	}
+	
+	public Tuple getNextTuple() throws IOException{
+		if(this.file.ready())
+			return new Tuple(this.file.readLine().split(","));
+		else{
+			this.file.close();
+			return null;
+		}
+			
+	}
+}
