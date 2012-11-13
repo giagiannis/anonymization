8c4e80c6ea0c55d104f09c8d0dba895338a575c5
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2012-11-13 10:44:24 +0200

    updated Runner

--- src/Runner.java
+++ src/Runner.java
@@ -15,6 +15,7 @@ public class Runner {
 
 	public static String args[];
 
+	/**	static method String get value for returning params	*/
 	public static String getValue(String value){
 		for(int i=0;i<args.length;i++)
 			if(args[i].equals(value))
