918d8b3bd1a3af81f3fd85fd05aeb2dfd990a725
Giannis Giannakopoulos <giannisgiannak@gmail.com>
2012-12-07 12:16:05 +0200

    added new centralized algorithm

---
+++ src/anonymity/algorithms/SBA.java
@@ -0,0 +1,13 @@
+package anonymity.algorithms;
+
+import anonymity.Algorithm;
+
+public class SBA extends Algorithm {
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
